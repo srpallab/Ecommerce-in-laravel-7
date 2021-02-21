b5a8dfc1ceaeb3fafbc4e9f2ec1077efa16838b9
Md. Shafiqur Rahman <pallab.alif@gmail.com>
2021-02-21 19:35:46 +0600

    Some Internal Changes

--- public/js/app.js
+++ public/js/app.js
@@ -66218,8 +66218,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/pallab/workspace/php_learning/laravel/Ecommerce-in-laravel-7/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/pallab/workspace/php_learning/laravel/Ecommerce-in-laravel-7/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/fnccombd/public_html/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/fnccombd/public_html/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
